d7584c1109d3255c8fa3b16973303314f7f23382
Funcrafter4 <funcrafter123456@gmail.com>
2023-11-17 11:20:42 +0600

    UseCirs Fix

--- ADBFinal/Program.cs
+++ ADBFinal/Program.cs
@@ -25,7 +25,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 void ConfigureServices(IServiceCollection services)
 {
     services.AddControllers();
